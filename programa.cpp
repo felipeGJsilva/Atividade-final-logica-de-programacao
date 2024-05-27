a625d38f1567639835a5e29bc971cbf3389481ad
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-05-27 22:01:38 +0000

    feito

--- programa.cpp
+++ programa.cpp
@@ -17,45 +17,47 @@ void adicionarTarefa() {
     cout << "adicionar Nova Tarefa\n";
 
     cout << "digite o ID: ";
-    cin >> ids[numTarefas];
+        cin >> ids[numTarefas];
     cin.ignore(); // Ignorar o caractere de nova linha deixado pelo `cin`.
 
     cout << "digite o titulo: ";
-    getline(cin, titulos[numTarefas]);
+        getline(cin, titulos[numTarefas]);
 
     cout << "digite a descrição: ";
-    getline(cin, descricao[numTarefas]);
+        getline(cin, descricao[numTarefas]);
 
     cout << "digite a data: ";
-    getline(cin, data[numTarefas]);
+        getline(cin, data[numTarefas]);
 
     cout << "status (1 - pendente, 2 - em Progresso, 3 - concluída): ";
-    cin >> status[numTarefas];
+        cin >> status[numTarefas];
 
     if (status[numTarefas] >= 1 && status[numTarefas] <= 3) {
         cout << "tarefa adicionada com sucesso!\n";
-        numTarefas++;
+            numTarefas++;
     } else {
         cout << "não foi possível salvar a tarefa\n";
-    }
+        }
      system("clear");
 }
 
 void visualizarTarefa() {
      system("clear");
     cout << "lista de tarefas\n";
-    for (int i = 0; i < numTarefas; i++) {
-        cout << "ID: " << ids[i] << ", titulo: " << titulos[i] << ", descrição: " << descricao[i]
-             << ", data de vencimento: " << data[i] << ", status: ";
+        for (int i = 0; i < numTarefas; i++) {
+            cout << "ID: " << ids[i] << ", titulo: " << titulos[i] << ", descrição: " << descricao[i]
+                << ", data de vencimento: " << data[i] << ", status: ";
         
         if (status[i] == 1) {
-            cout << "pendente";
-        } else if (status[i] == 2) {
-            cout << "em progresso";
-        } else if (status[i] == 3) {
-            cout << "concluída";
-        }
-        cout << "\n";
+                cout << "pendente";
+            }
+        else if (status[i] == 2) {
+                cout << "em progresso";
+            } 
+        else if (status[i] == 3) {
+                cout << "concluída";
+            }
+            cout << "\n";
         
     }
     
@@ -82,6 +84,45 @@ void editarTarefa(){
     }
 }
 
+void removerTarefa() {
+    int id;
+    cout << "Remover Tarefa\n";
+    cout << "ID da Tarefa: ";
+    cin >> id;
+    for (int i = 0; i < numTarefas; i++) {
+        if (ids[i] == id) {
+            for (int j = i; j < numTarefas - 1; j++) {
+                ids[j] = ids[j + 1];
+                titulos[j] = titulos[j + 1];
+                descricao[j] = descricao[j + 1];
+                data[j] = data[j + 1];
+                status[j] = status[j + 1];
+            }
+            numTarefas--;
+            cout << "Tarefa removida com sucesso!\n";
+            return;
+        }
+    }
+    cout << "Tarefa não encontrada.\n";
+}
+void buscarTarefa() {
+    string titulo;
+    cout << "Buscar Tarefa\n";
+    cin.ignore();
+    cout << "Título da Tarefa: ";
+    getline(cin, titulo);
+    for (int i = 0; i < numTarefas; i++) {
+        if (titulos[i] == titulo) {
+            cout << "Resultado da busca:\n";
+            cout << "ID: " << ids[i] << ", Título: " << titulos[i] << ", Descrição: " << descricao[i];
+
+        }
+    }
+}
+
+
+
+
 int main() {
     int opcao;
 
@@ -96,7 +137,7 @@ int main() {
         cout << "0. Sair\n";
         cout << "Escolha uma opção: ";
         cin >> opcao;
-      system("clear");
+      
         if (opcao == 1) {
             adicionarTarefa();
         } else if (opcao == 2) {
@@ -105,6 +146,14 @@ int main() {
         else if(opcao == 3){
             editarTarefa();
         }
+         else if(opcao == 4){
+            removerTarefa();
+        }
+         else if(opcao == 5){
+            buscarTarefa();
+        }
+       
+        
         else if (opcao == 0) {
             cout << "Saindo...\n";
         }
